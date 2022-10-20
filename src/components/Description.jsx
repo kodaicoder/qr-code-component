64d24d74c46eca7625fe2e302466a42fdb4c2bd4
kodaicoder <nutdeknon@gmail.com>
2022-10-20 22:39:27 +0700

    fix bg color and h1 size

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export const Description = () => {
   return (
     <div className="p-8 pt-0">
-      <h1 className="font-[700] text-xl text-[color:var(--dark-blue)]">
+      <h1 className="font-[700] text-2xl text-[color:var(--dark-blue)]">
         Improve your front-end skills by building projects
       </h1>
-      <p className="mt-4 font-[400] text-[color:var(--grayish-blue)]">
+      <p className="mt-4 font-[400]">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
